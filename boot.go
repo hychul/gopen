395abbc3c4f0c1ef1f7cd0e18bad8e1f2813d70b
hychul <hychome@gmail.com>
2023-10-09 23:36:42 +0900

    Create test loop with goroutine

--- boot.go
+++ boot.go
@@ -1,6 +1,10 @@
 package gopen
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 type App interface {
 	Update()
@@ -8,4 +12,33 @@ type App interface {
 
 func Run() {
 	fmt.Println("Hello Gopen!")
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go logicLoop(&wg)
+	go renderLoop(&wg)
+
+	wg.Wait()
+
+	fmt.Println("Test Loop Finished")
+}
+
+func logicLoop(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("Logic Loop Iteration:", i)
+		time.Sleep(time.Second)
+	}
+}
+
+func renderLoop(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("Render Loop Iteration:", i)
+		time.Sleep(500 * time.Millisecond)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/hychul/gopen
 
 go 1.21.1
+
+require (
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b
+)
+
+require (
+	github.com/go-gl/mathgl v1.1.0 // indirect
+	golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f // indirect
+)
