bf0d28e0f59a8b9858f3fcf1757c641060c35d61
hychul <hychome@gmail.com>
2023-10-12 00:45:25 +0900

    Refactoring gl41 example

--- examples/gl41-cube/main.go
+++ examples/gl41-cube/main.go
@@ -21,8 +21,107 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 )
 
-const windowWidth = 800
-const windowHeight = 600
+const (
+	windowWidth, windowHeight = 800, 600
+
+	vertexShader = `
+#version 330
+
+uniform mat4 projection;
+uniform mat4 camera;
+uniform mat4 model;
+
+in vec3 vert;
+in vec2 vertTexCoord;
+
+out vec2 fragTexCoord;
+
+void main() {
+    fragTexCoord = vertTexCoord;
+    gl_Position = projection * camera * model * vec4(vert, 1);
+}
+` + "\x00"
+
+	fragmentShader = `
+#version 330
+
+uniform sampler2D tex;
+
+in vec2 fragTexCoord;
+
+out vec4 outputColor;
+
+void main() {
+    outputColor = texture(tex, fragTexCoord);
+}
+` + "\x00"
+)
+
+var (
+	cubeVertices = []float32{
+		//  X, Y, Z, U, V
+		// Bottom
+		-1.0, -1.0, -1.0, 0.0, 0.0,
+		1.0, -1.0, -1.0, 1.0, 0.0,
+		-1.0, -1.0, 1.0, 0.0, 1.0,
+		1.0, -1.0, -1.0, 1.0, 0.0,
+		1.0, -1.0, 1.0, 1.0, 1.0,
+		-1.0, -1.0, 1.0, 0.0, 1.0,
+
+		// Top
+		-1.0, 1.0, -1.0, 0.0, 0.0,
+		-1.0, 1.0, 1.0, 0.0, 1.0,
+		1.0, 1.0, -1.0, 1.0, 0.0,
+		1.0, 1.0, -1.0, 1.0, 0.0,
+		-1.0, 1.0, 1.0, 0.0, 1.0,
+		1.0, 1.0, 1.0, 1.0, 1.0,
+
+		// Front
+		-1.0, -1.0, 1.0, 1.0, 0.0,
+		1.0, -1.0, 1.0, 0.0, 0.0,
+		-1.0, 1.0, 1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0, 0.0, 0.0,
+		1.0, 1.0, 1.0, 0.0, 1.0,
+		-1.0, 1.0, 1.0, 1.0, 1.0,
+
+		// Back
+		-1.0, -1.0, -1.0, 0.0, 0.0,
+		-1.0, 1.0, -1.0, 0.0, 1.0,
+		1.0, -1.0, -1.0, 1.0, 0.0,
+		1.0, -1.0, -1.0, 1.0, 0.0,
+		-1.0, 1.0, -1.0, 0.0, 1.0,
+		1.0, 1.0, -1.0, 1.0, 1.0,
+
+		// Left
+		-1.0, -1.0, 1.0, 0.0, 1.0,
+		-1.0, 1.0, -1.0, 1.0, 0.0,
+		-1.0, -1.0, -1.0, 0.0, 0.0,
+		-1.0, -1.0, 1.0, 0.0, 1.0,
+		-1.0, 1.0, 1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0, 1.0, 0.0,
+
+		// Right
+		1.0, -1.0, 1.0, 1.0, 1.0,
+		1.0, -1.0, -1.0, 1.0, 0.0,
+		1.0, 1.0, -1.0, 0.0, 0.0,
+		1.0, -1.0, 1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0, 0.0, 0.0,
+		1.0, 1.0, 1.0, 0.0, 1.0,
+	}
+
+	program uint32
+
+	model        mgl32.Mat4
+	modelUniform int32
+
+	vao uint32
+
+	texture uint32
+
+	angle float64
+
+	previousTime float64
+)
 
 func init() {
 	// GLFW event handling must run on the main OS thread
@@ -36,6 +135,7 @@ func main() {
 	defer glfw.Terminate()
 
 	glfw.WindowHint(glfw.Resizable, glfw.False)
+	// Set GL version
 	glfw.WindowHint(glfw.ContextVersionMajor, 4)
 	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
@@ -46,92 +146,30 @@ func main() {
 	}
 	window.MakeContextCurrent()
 
-	// Initialize Glow
+	// Initialize Gl
 	if err := gl.Init(); err != nil {
 		panic(err)
 	}
 
+	// Log GL version
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
 
 	// Configure the vertex and fragment shaders
-	program, err := newProgram(vertexShader, fragmentShader)
+	program, err = newProgram(vertexShader, fragmentShader)
 	if err != nil {
 		panic(err)
 	}
 
-	gl.UseProgram(program)
-
-	projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(windowWidth)/windowHeight, 0.1, 10.0)
-	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
-	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
-
-	camera := mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
-	cameraUniform := gl.GetUniformLocation(program, gl.Str("camera\x00"))
-	gl.UniformMatrix4fv(cameraUniform, 1, false, &camera[0])
-
-	model := mgl32.Ident4()
-	modelUniform := gl.GetUniformLocation(program, gl.Str("model\x00"))
-	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
-
-	textureUniform := gl.GetUniformLocation(program, gl.Str("tex\x00"))
-	gl.Uniform1i(textureUniform, 0)
-
-	gl.BindFragDataLocation(program, 0, gl.Str("outputColor\x00"))
-
 	// Load the texture
-	texture, err := newTexture("square.png")
+	texture, err = newTexture("square.png")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// Configure the vertex data
-	var vao uint32
-	gl.GenVertexArrays(1, &vao)
-	gl.BindVertexArray(vao)
-
-	var vbo uint32
-	gl.GenBuffers(1, &vbo)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData(gl.ARRAY_BUFFER, len(cubeVertices)*4, gl.Ptr(cubeVertices), gl.STATIC_DRAW)
-
-	vertAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vert\x00")))
-	gl.EnableVertexAttribArray(vertAttrib)
-	gl.VertexAttribPointerWithOffset(vertAttrib, 3, gl.FLOAT, false, 5*4, 0)
-
-	texCoordAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vertTexCoord\x00")))
-	gl.EnableVertexAttribArray(texCoordAttrib)
-	gl.VertexAttribPointerWithOffset(texCoordAttrib, 2, gl.FLOAT, false, 5*4, 3*4)
-
-	// Configure global settings
-	gl.Enable(gl.DEPTH_TEST)
-	gl.DepthFunc(gl.LESS)
-	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
-
-	angle := 0.0
-	previousTime := glfw.GetTime()
-
+	setupScene()
 	for !window.ShouldClose() {
-		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-		// Update
-		time := glfw.GetTime()
-		elapsed := time - previousTime
-		previousTime = time
-
-		angle += elapsed
-		model = mgl32.HomogRotate3D(float32(angle), mgl32.Vec3{0, 1, 0})
-
-		// Render
-		gl.UseProgram(program)
-		gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
-
-		gl.BindVertexArray(vao)
-
-		gl.ActiveTexture(gl.TEXTURE0)
-		gl.BindTexture(gl.TEXTURE_2D, texture)
-
-		gl.DrawArrays(gl.TRIANGLES, 0, 6*2*3)
+		drawScene()
 
 		// Maintenance
 		window.SwapBuffers()
@@ -214,8 +252,8 @@ func newTexture(file string) (uint32, error) {
 	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
 
 	var texture uint32
-	gl.GenTextures(1, &texture)
 	gl.ActiveTexture(gl.TEXTURE0)
+	gl.GenTextures(1, &texture)
 	gl.BindTexture(gl.TEXTURE_2D, texture)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
@@ -235,87 +273,76 @@ func newTexture(file string) (uint32, error) {
 	return texture, nil
 }
 
-var vertexShader = `
-#version 330
+func setupScene() {
+	// Setup variables
+	previousTime = glfw.GetTime()
+	angle = 0.0
 
-uniform mat4 projection;
-uniform mat4 camera;
-uniform mat4 model;
+	// Configure the shader program
+	gl.UseProgram(program)
 
-in vec3 vert;
-in vec2 vertTexCoord;
+	projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(windowWidth)/windowHeight, 0.1, 10.0)
+	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
+	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
 
-out vec2 fragTexCoord;
+	camera := mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
+	cameraUniform := gl.GetUniformLocation(program, gl.Str("camera\x00"))
+	gl.UniformMatrix4fv(cameraUniform, 1, false, &camera[0])
 
-void main() {
-    fragTexCoord = vertTexCoord;
-    gl_Position = projection * camera * model * vec4(vert, 1);
+	model = mgl32.Ident4()
+	modelUniform = gl.GetUniformLocation(program, gl.Str("model\x00"))
+	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
+
+	textureUniform := gl.GetUniformLocation(program, gl.Str("tex\x00"))
+	gl.Uniform1i(textureUniform, 0)
+
+	gl.BindFragDataLocation(program, 0, gl.Str("outputColor\x00"))
+
+	// Configure the vertex data
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(cubeVertices)*4, gl.Ptr(cubeVertices), gl.STATIC_DRAW)
+
+	vertAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vert\x00")))
+	gl.EnableVertexAttribArray(vertAttrib)
+	gl.VertexAttribPointerWithOffset(vertAttrib, 3, gl.FLOAT, false, 5*4, 0)
+
+	texCoordAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vertTexCoord\x00")))
+	gl.EnableVertexAttribArray(texCoordAttrib)
+	gl.VertexAttribPointerWithOffset(texCoordAttrib, 2, gl.FLOAT, false, 5*4, 3*4)
+
+	// Configure global settings
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
 }
-` + "\x00"
 
-var fragmentShader = `
-#version 330
+func drawScene() {
+	// Update
+	time := glfw.GetTime()
+	elapsed := time - previousTime
+	previousTime = time
 
-uniform sampler2D tex;
+	angle += elapsed
 
-in vec2 fragTexCoord;
+	model = mgl32.HomogRotate3D(float32(angle), mgl32.Vec3{0, 1, 0})
 
-out vec4 outputColor;
+	// Render
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-void main() {
-    outputColor = texture(tex, fragTexCoord);
-}
-` + "\x00"
+	gl.UseProgram(program)
+	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
 
-var cubeVertices = []float32{
-	//  X, Y, Z, U, V
-	// Bottom
-	-1.0, -1.0, -1.0, 0.0, 0.0,
-	1.0, -1.0, -1.0, 1.0, 0.0,
-	-1.0, -1.0, 1.0, 0.0, 1.0,
-	1.0, -1.0, -1.0, 1.0, 0.0,
-	1.0, -1.0, 1.0, 1.0, 1.0,
-	-1.0, -1.0, 1.0, 0.0, 1.0,
+	gl.BindVertexArray(vao)
 
-	// Top
-	-1.0, 1.0, -1.0, 0.0, 0.0,
-	-1.0, 1.0, 1.0, 0.0, 1.0,
-	1.0, 1.0, -1.0, 1.0, 0.0,
-	1.0, 1.0, -1.0, 1.0, 0.0,
-	-1.0, 1.0, 1.0, 0.0, 1.0,
-	1.0, 1.0, 1.0, 1.0, 1.0,
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, texture)
 
-	// Front
-	-1.0, -1.0, 1.0, 1.0, 0.0,
-	1.0, -1.0, 1.0, 0.0, 0.0,
-	-1.0, 1.0, 1.0, 1.0, 1.0,
-	1.0, -1.0, 1.0, 0.0, 0.0,
-	1.0, 1.0, 1.0, 0.0, 1.0,
-	-1.0, 1.0, 1.0, 1.0, 1.0,
-
-	// Back
-	-1.0, -1.0, -1.0, 0.0, 0.0,
-	-1.0, 1.0, -1.0, 0.0, 1.0,
-	1.0, -1.0, -1.0, 1.0, 0.0,
-	1.0, -1.0, -1.0, 1.0, 0.0,
-	-1.0, 1.0, -1.0, 0.0, 1.0,
-	1.0, 1.0, -1.0, 1.0, 1.0,
-
-	// Left
-	-1.0, -1.0, 1.0, 0.0, 1.0,
-	-1.0, 1.0, -1.0, 1.0, 0.0,
-	-1.0, -1.0, -1.0, 0.0, 0.0,
-	-1.0, -1.0, 1.0, 0.0, 1.0,
-	-1.0, 1.0, 1.0, 1.0, 1.0,
-	-1.0, 1.0, -1.0, 1.0, 0.0,
-
-	// Right
-	1.0, -1.0, 1.0, 1.0, 1.0,
-	1.0, -1.0, -1.0, 1.0, 0.0,
-	1.0, 1.0, -1.0, 0.0, 0.0,
-	1.0, -1.0, 1.0, 1.0, 1.0,
-	1.0, 1.0, -1.0, 0.0, 0.0,
-	1.0, 1.0, 1.0, 0.0, 1.0,
+	gl.DrawArrays(gl.TRIANGLES, 0, 6*2*3)
 }
 
 // Set the working directory to the root of Go package, so that its assets can be accessed.
